608709ce79d098e4109d17077470914a52d8d3bd
Rayssa <158166558+raygoncalvesss@users.noreply.github.com>
2024-10-24 16:56:39 -0300

    feat: create get all users

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -4,4 +4,12 @@ const candidatosRoutes = Router();
 
 let usuarios = []
 
+usuariosRoutes.get("/", (req, res) => {
+    return res.status(200).json({
+        message: 
+        usuarios.length == 0
+        ? "Não há usuarios cadastrados" : `Total de usuarios: ${usuarios.length}`,
+        usuarios,
+    });
+});
 export default candidatosRoutes;
